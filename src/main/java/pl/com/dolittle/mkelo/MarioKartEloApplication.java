3c81538fc3a93e3f41420ddf012e23f15e2eb4f4
mapuchalski <mapuchalski@deloitte.de>
2022-02-24 11:14:52 +0100

    Initial commit.

---
+++ src/main/java/pl/com/dolittle/mkelo/MarioKartEloApplication.java
@@ -0,0 +1,13 @@
+package pl.com.dolittle.mkelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioKartEloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioKartEloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/dolittle/mkelo/MarioKartEloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.dolittle.mkelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioKartEloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
