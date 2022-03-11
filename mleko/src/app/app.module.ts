12b32b3e090251013c15a073413f9e204cdacd8a
Kamil Sacharczuk <ksacharczuk@deloittece.com>
2022-03-11 09:52:22 +0100

    user-add draft

--- mleko/src/app/app.module.ts
+++ mleko/src/app/app.module.ts
@@ -1,21 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {RouterModule, Routes} from "@angular/router";
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterModule, Routes } from "@angular/router";
 import { LastResultsComponent } from './last-results/last-results.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 const appRoutes: Routes = [
   { path: 'last-results', component: LastResultsComponent },
+  { path: 'user-add', component: UserAddComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LastResultsComponent
+    LastResultsComponent,
+    UserAddComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +31,10 @@ const appRoutes: Routes = [
       { enableTracing: true } // <-- debugging purposes only
     ),
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
