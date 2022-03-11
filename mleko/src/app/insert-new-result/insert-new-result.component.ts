adb774c4ff21d4807af0b70e8b75fb6a06d2c88f
Artur Kondziela <Artur.Kondziela@partner.bmw.de>
2022-03-11 10:55:48 +0100

    Add notification that no secret

--- mleko/src/app/insert-new-result/insert-new-result.component.ts
+++ mleko/src/app/insert-new-result/insert-new-result.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 import {MlekoService} from "src/app/service/mleko.service";
 import {Player, Result} from "src/app/model/models";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-insert-new-result',
@@ -10,6 +11,8 @@ import {Player, Result} from "src/app/model/models";
 })
 export class InsertNewResultComponent implements OnInit {
 
+  // @ts-ignore
+  secret: string;
   players: Array<Player> = [];
   firstPlace: Array<Player> = [];
   secondPlace: Array<Player> = [];
@@ -17,8 +20,15 @@ export class InsertNewResultComponent implements OnInit {
   fourthPlace: Array<Player> = [];
 
   constructor(
-    private mlekoService: MlekoService
-  ) { }
+    private mlekoService: MlekoService,
+    private activationRoute: ActivatedRoute
+  ) {
+    this.activationRoute.queryParams.subscribe(params => {
+      if (params['secret']) {
+        this.secret = params['secret'];
+      }
+    })
+  }
 
   ngOnInit(): void {
     this.mlekoService.getPlayers()
@@ -40,7 +50,7 @@ export class InsertNewResultComponent implements OnInit {
 
   saveResult() {
     const result: Result = {
-      reportedBySecret: '',
+      reportedBySecret: this.secret,
       results: [
         this.firstPlace.map(place => place.uuid),
         this.secondPlace.map(place => place.uuid),
