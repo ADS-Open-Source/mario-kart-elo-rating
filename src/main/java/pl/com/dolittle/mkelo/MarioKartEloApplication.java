631e63d7e272e56cb2e39779a7c7d04145eb13a0
Lukasz Mowinski <lmowinski@deloittece.com>
2022-11-28 09:51:44 +0100

    feat(config): added config package

--- src/main/java/pl/com/dolittle/mkelo/MarioKartEloApplication.java
+++ src/main/java/pl/com/dolittle/mkelo/MarioKartEloApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarioKartEloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarioKartEloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarioKartEloApplication.class, args);
+    }
 
 }
---
+++ src/main/java/pl/com/dolittle/mkelo/config/WebRoutingConfig.java
@@ -0,0 +1,48 @@
+package pl.com.dolittle.mkelo.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebRoutingConfig implements WebMvcConfigurer {
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/urlNotFound")
+                // the viewName should be specified, in our case we forward to the index.html
+                .setViewName("forward:/index.html");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/urlNotFound"));
+    }
+
+    @Bean
+    public AmazonS3 getS3client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.EU_CENTRAL_1).build();
+    }
+}
