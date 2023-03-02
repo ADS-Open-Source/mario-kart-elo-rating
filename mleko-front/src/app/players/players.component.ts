5597a7fcd0bc85fc3977b6b984f1c61827bd5a7c
Lukasz Mowinski <lmowinski@deloittece.com>
2023-03-02 17:27:40 +0100

    feat: button and resendEmail stud created

--- mleko-front/src/app/players/players.component.ts
+++ mleko-front/src/app/players/players.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {Player} from "../models/Player";
 import {MlekoService} from "../services/mleko.service";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-players',
@@ -12,6 +13,9 @@ export class PlayersComponent implements OnInit {
 
   playersSub!: Subscription;
   allPlayers: Player[] = [];
+  dataSource: MatTableDataSource<Player> = new MatTableDataSource<Player>();
+  displayedColumns: string[] = ['username', 'resend'];
+  isProcessing: boolean = false;
 
   constructor(
     private mlekoService: MlekoService,
@@ -22,6 +26,7 @@ export class PlayersComponent implements OnInit {
     this.playersSub = this.mlekoService.$playersStore
       .subscribe((players: Player[]) => {
         this.allPlayers = players.sort((a, b) => a.name.localeCompare(b.name));
+        this.dataSource.data = this.allPlayers;
       });
   }
 
@@ -29,4 +34,8 @@ export class PlayersComponent implements OnInit {
     this.updateAllPlayers();
   }
 
+  resendEmail(username: string) {
+    this.isProcessing = true;
+    console.log(username)// TODO finish resend Email
+  }
 }
