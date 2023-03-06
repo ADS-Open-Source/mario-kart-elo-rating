9ead0b6f8e8457445fcbe1a7da389ad4f676b035
Lukasz Mowinski <lmowinski@deloittece.com>
2023-03-06 15:14:42 +0100

    feat: screen service created

--- mleko-front/src/app/app.component.ts
+++ mleko-front/src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ScreenSizeService} from "./services/screen-size-service.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  deviceScreenSize: number = 2048;
+
+  constructor(
+    private screenSizeService: ScreenSizeService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.deviceScreenSize = window.screen.width;
+    this.screenSizeService.setIfDesktop(this.deviceScreenSize);
+  }
 }
